334bd48d9a9af385ccc8cb6057035a0e390a771a
Marco F <marco.friso@gmail.com>
2023-02-01 10:42:43 +0100

    feat: implement accordion functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import tableData from "./data/example-data.json";
 
 const App = () => {
+  const [openKidsGroupUUIDs, setOpenKidsGroupUUIDs] = useState([]);
+
   return (
     <div className="App">
-      <Table tableData={tableData} name="Data Table" />
+      <Table
+        tableData={tableData}
+        name="Data Table"
+        openKidsGroupUUIDs={openKidsGroupUUIDs}
+        setOpenKidsGroupUUIDs={setOpenKidsGroupUUIDs}
+      />
     </div>
   );
 };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,36 +1,100 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { v4 as uuid } from "uuid";
 
-const Table = ({ tableData, name }) => {
+const Table = ({
+  tableData,
+  name,
+  openKidsGroupUUIDs,
+  setOpenKidsGroupUUIDs,
+}) => {
   const headerGroups = Object.keys(tableData[0].data);
+
+  const toggleAccordion = (kidsGroupUUID) => {
+    if (openKidsGroupUUIDs.includes(kidsGroupUUID)) {
+      setOpenKidsGroupUUIDs(
+        openKidsGroupUUIDs.filter((uuid) => uuid !== kidsGroupUUID)
+      );
+    } else {
+      // console.log(1, [...openKidsGroupUUIDs, kidsGroupUUID]);
+      // console.log(2, [...openKidsGroupUUIDs]);
+      // console.log(3, kidsGroupUUID);
+      setOpenKidsGroupUUIDs([...openKidsGroupUUIDs, kidsGroupUUID]);
+    }
+  };
 
   return (
     <>
-      <p>{name.toUpperCase()}</p>
-      <table>
-        <thead>
+      <p className="fw-bold">{name.toUpperCase()}</p>
+      <table className="table">
+        <thead className="bg-light">
           <tr>
+            <th scope="col" className="accordion-button" />
             {headerGroups.map((headerGroup) => (
-              <th key={uuid()}>{headerGroup}</th>
+              <th scope="col" key={uuid()}>
+                {headerGroup}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
           {tableData.map((row) => (
             <Fragment key={uuid()}>
-              <tr>
-                {headerGroups.map((val) => (
-                  <td key={uuid()}>{row.data[val]}</td>
-                ))}
-              </tr>
-              {Object.keys(row.kids) &&
-                Object.keys(row.kids).map((val) => (
-                  <tr key={uuid()}>
-                    <td colSpan={headerGroups.length}>
-                      <Table tableData={row.kids[val].records} name={val} />
-                    </td>
-                  </tr>
-                ))}
+              {Object.keys(row.kids).length ? (
+                (() => {
+                  const kidsGroupUUID = `${headerGroups[0]}-${
+                    row.data[headerGroups[0]]
+                  }`;
+                  const openAccordion =
+                    openKidsGroupUUIDs.includes(kidsGroupUUID);
+
+                  return (
+                    <>
+                      <tr>
+                        <td
+                          className="accordion-button"
+                          onClick={() => toggleAccordion(kidsGroupUUID)}
+                        >
+                          <i
+                            className={
+                              openAccordion
+                                ? "fa fa-chevron-down"
+                                : "fa fa-chevron-right"
+                            }
+                          />
+                        </td>
+                        {headerGroups.map((val) => (
+                          <td key={uuid()}>{row.data[val]}</td>
+                        ))}
+                      </tr>
+                      {openAccordion &&
+                        Object.keys(row.kids).map((val) => (
+                          <tr
+                            key={uuid()}
+                            display={
+                              openAccordion ? "table-row" : "display-none"
+                            }
+                          >
+                            <td colSpan={headerGroups.length}>
+                              <Table
+                                tableData={row.kids[val].records}
+                                name={val}
+                                openKidsGroupUUIDs={openKidsGroupUUIDs}
+                                setOpenKidsGroupUUIDs={setOpenKidsGroupUUIDs}
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                    </>
+                  );
+                })()
+              ) : (
+                <tr>
+                  <td className="accordion-button" />
+                  {headerGroups.map((val) => (
+                    <td key={uuid()}>{row.data[val]}</td>
+                  ))}
+                </tr>
+              )}
             </Fragment>
           ))}
         </tbody>
